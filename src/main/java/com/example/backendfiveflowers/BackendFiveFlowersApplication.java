3e436e0258f9499f245ed2ccd2a968969347f49e
zukahm2005 <145833253+zukahm2005@users.noreply.github.com>
2024-06-17 13:59:54 +0700

    .

---
+++ src/main/java/com/example/backendfiveflowers/BackendFiveFlowersApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendfiveflowers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFiveFlowersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFiveFlowersApplication.class, args);
+	}
+
+}
