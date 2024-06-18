2c69b5adeb75255b71392f15df4d0f57deb7f776
PhanQuan2308 <hpq833592@gmail.com>
2024-06-18 16:38:11 +0700

    pull de

--- src/main/java/com/example/backendfiveflowers/Filter/JwtRequestFilter.java
+++ src/main/java/com/example/backendfiveflowers/Filter/JwtRequestFilter.java
@@ -1,6 +1,10 @@
 package com.example.backendfiveflowers.Filter;
 
 import com.example.backendfiveflowers.service.TokenBlacklistService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -10,10 +14,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Component
