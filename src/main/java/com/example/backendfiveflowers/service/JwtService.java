d05a351d5818936b18b722e0a55e476c61d7e5cc
PhanQuan2308 <hpq833592@gmail.com>
2024-07-01 13:21:42 +0700

    add image success

--- src/main/java/com/example/backendfiveflowers/service/JwtService.java
+++ src/main/java/com/example/backendfiveflowers/service/JwtService.java
@@ -38,7 +38,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30)) // 30 minutes expiration
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 30 * 30)) // 30 minutes expiration
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
